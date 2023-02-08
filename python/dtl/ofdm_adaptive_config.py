73338548b184b76789db7a6fbfd5babed4e4f8a1
Mihai Stef <mstef@tillit.ai>
2023-02-08 13:05:40 +0200

    add known constellations to config

--- python/dtl/ofdm_adaptive_config.py
+++ python/dtl/ofdm_adaptive_config.py
@@ -1,20 +1,24 @@
 import dataclasses as dc
 from gnuradio import (
     digital,
+    dtl,
 )
+import sys
 import typing as t
+
 
 @dc.dataclass
 class ofdm_adaptive_config:
-    fft_len : int = 64
-    cp_len : int = 16
-    frame_length_tag_key : str = "frame_length"
-    packet_length_tag_key : str = "packet_length"
-    packet_num_tag_key : str = "packet_num"
-    occupied_carriers : t.Tuple[t.List[int]] = (list(range(-26, -21)) + list(range(-20, -7)) + list(
+    fft_len: int = 64
+    cp_len: int = 16
+    frame_length_tag_key: str = "frame_length"
+    packet_length_tag_key: str = "packet_length"
+    packet_num_tag_key: str = "packet_num"
+    occupied_carriers: t.Tuple[t.List[int]] = (list(range(-26, -21)) + list(range(-20, -7)) + list(
         range(-6, 0)) + list(range(1, 7)) + list(range(8, 21)) + list(range(22, 27)),)
-    pilot_carriers : t.Tuple[t.Tuple[int]] = ((-21, -7, 7, 21,), (-21, -7, 7, 21,))
-    pilot_sym_scramble_seq : t.Tuple[int] = (
+    pilot_carriers: t.Tuple[t.Tuple[int]] = (
+        (-21, -7, 7, 21,), (-21, -7, 7, 21,))
+    pilot_sym_scramble_seq: t.Tuple[int] = (
         1, 1, 1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, 1, -1, 1, -
         1, -1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1, 1, -1, 1,
         1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, 1, -1, -1, -1, 1, -
@@ -24,21 +28,25 @@ class ofdm_adaptive_config:
         -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1, 1, 1, 1, -1, -
         1, 1, -1, -1, -1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1
     )
-    pilot_symbols : t.Tuple[int] = tuple([(x, x, x, -x) for x in pilot_sym_scramble_seq])
-    sync_word1 : list = digital.ofdm_txrx._make_sync_word1(
+    pilot_symbols: t.Tuple[int] = tuple(
+        [(x, x, x, -x) for x in pilot_sym_scramble_seq])
+    sync_word1: list = digital.ofdm_txrx._make_sync_word1(
         fft_len, occupied_carriers, pilot_carriers)
-    sync_word2 : list = digital.ofdm_txrx._make_sync_word2(
+    sync_word2: list = digital.ofdm_txrx._make_sync_word2(
         fft_len, occupied_carriers, pilot_carriers)
-    rolloff : int = 0
-    debug : bool = False
-    debug_folder : str = "/tmp/debug"
-    scramble_bits : bool = False
-    frame_length : int = 20
+    rolloff: int = 0
+    debug: bool = False
+    debug_folder: str = "/tmp/debug"
+    scramble_bits: bool = False
+    frame_length: int = 20
+    constellations: t.Tuple[t.Tuple[float, dtl.constellation_type_t]] = ((sys.float_info.min, dtl.constellation_type_t.BPSK), (
+        15, dtl.constellation_type_t.QPSK), (18, dtl.constellation_type_t.PSK8), (21, dtl.constellation_type_t.QAM16),)
 
 
 @dc.dataclass
 class ofdm_adaptive_tx_config(ofdm_adaptive_config):
     pass
+
 
 @dc.dataclass
 class ofdm_adaptive_rx_config(ofdm_adaptive_config):
--- python/dtl/qa_ofdm_adaptive_frame_equalizer_vcvc.py
+++ python/dtl/qa_ofdm_adaptive_frame_equalizer_vcvc.py
@@ -22,6 +22,7 @@ try:
         constellation_type_t,
         get_constellation_tag_key,
         estimated_snr_tag_key,
+        ofdm_adaptive_config,
         ofdm_adaptive_frame_equalizer_vcvc,
         ofdm_adaptive_equalizer,
         ofdm_adaptive_frame_snr_simple,
@@ -36,6 +37,7 @@ except ImportError:
         constellation_type_t,
         get_constellation_tag_key,
         estimated_snr_tag_key,
+        ofdm_adaptive_config,
         ofdm_adaptive_frame_equalizer_vcvc,
         ofdm_adaptive_equalizer,
         ofdm_adaptive_frame_snr_simple,
@@ -106,7 +108,8 @@ class qa_ofdm_adaptive_frame_equalizer_vcvc(gr_unittest.TestCase):
                 rx_signal, False, fft_len, (chan_tag, const_tag))
             feedback_decision_sink = blocks.message_debug()
             eq = ofdm_adaptive_frame_equalizer_vcvc(
-                equalizer.base(), ofdm_adaptive_feedback_decision(1, 3), 0, "tsb_key", True, True)
+                equalizer.base(), ofdm_adaptive_feedback_decision(
+                    1, 3, ofdm_adaptive_config.ofdm_adaptive_config.constellations), 0, "tsb_key", True, True)
             self.tb.msg_connect(eq, 'feedback_port',
                                 feedback_decision_sink, 'store')
             sink = blocks.tsb_vector_sink_c(fft_len, tsb_key="tsb_key")
@@ -137,7 +140,8 @@ class qa_ofdm_adaptive_frame_equalizer_vcvc(gr_unittest.TestCase):
                           t.key for t in sink.tags()])
             self.assertEqual(feedback_decision_sink.num_messages(), 1)
             feedback_msg = feedback_decision_sink.get_message(0)
-            self.assertEqual(pmt.u8vector_elements(pmt.cdr(feedback_msg)), [int(c), 0])
+            self.assertEqual(pmt.u8vector_elements(
+                pmt.cdr(feedback_msg)), [int(c), 0])
 
 
 if __name__ == '__main__':
